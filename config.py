71ea7cf7a03ed8e2bd64bb3a388f277d36c2fea3
Justin Vanderhooft <justinvdhooft@gmail.com>
2015-10-19 14:56:51 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+# Plex Credentials
+PLEX_USERNAME = ''
+PLEX_PASSWORD = ''
+
+# Wink account credentials
+WINK_USERNAME = ''
+WINK_PASSWORD = ''
+
+# Wink API credentials. E-mail support@winkapp.com to request
+WINK_CLIENT_ID = ''
+WINK_CLIENT_SECRET = ''
+
+# The name of the client that should trigger a lighting event
+PLEX_CLIENT_TRIGGER_NAME = ''
+
+# The lighting group that should be triggered
+WINK_ACTION_GROUP = ''
