ff0939ba5f0fa736d764ed8052ea25aadfdecfec
Justin Vanderhooft <justinvdhooft@gmail.com>
2015-10-22 16:33:12 -0400

    updating config to support new options

--- config.py
+++ config.py
@@ -10,11 +10,14 @@ WINK_PASSWORD = ''
 WINK_CLIENT_ID = ''
 WINK_CLIENT_SECRET = ''
 
-# The name of the client that should trigger a lighting event
-PLEX_CLIENT_TRIGGER_NAME = ''
+# The name of the clients that should trigger a lighting event
+PLEX_CLIENTS = ['Living Room', 'Bedroom', 'Kitchen']
 
-# The lighting group that should be triggered
-WINK_ACTION_GROUP = ''
+# List of usernames that should trigger the lighting event
+PLEX_AUTHORIZED_USERS = ['user1', 'user2']
+
+# The lighting groups that should be triggered
+WINK_ACTION_GROUPS = ['Entryway Hallay', 'Living Room']
 
 # The path to your plex server including port
 PLEX_HTTP_PATH = '127.0.0.1:32400'
